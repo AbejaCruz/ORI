cf6f084c5cb77e4dc9b548de3a6e4a6fd6eb75fa
AbejaCruz <abeja.dact_@hotmail.com>
2017-02-04 23:45:32 -0500

    no message

---
+++ Assets/Script/Load_Game.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Load_Game : MonoBehaviour {
+
+	GameObject MStart;
+	Camera CameraStart;
+	public Vector3 CameraGamePosition; 
+	public Quaternion CameraGameRotation;
+	private StartGame sgame;
+	//private Button BtnPlay;
+	private bool playing=false;
+	// Use this for initialization
+
+	void Awake(){
+
+
+
+		CameraStart = ((GameObject)Instantiate(Resources.Load("Prefabs/CameraStart"), Vector3.zero, Quaternion.identity)).GetComponent<Camera>();
+		CameraStart.name = "CameraStart";
+		sgame = new StartGame();
+
+		MStart = (GameObject)Instantiate(Resources.Load("Prefabs/MStart"), transform.position, transform.rotation);
+		MStart.name = "MStart";
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Script/StartGame.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour {
+
+	public GameObject MStart;
+	public GameObject city;
+	public GameObject player;
+
+	public Camera CameraStart;
+	public Camera CameraGame;
+
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void startGame(){
+
+		//GameObject.Find("HUD").active = false;
+		GameObject.Find ("MStart").SetActive(false);
+
+		CameraStart=  GameObject.Find ("CameraStart").GetComponent<Camera>();
+		//CameraGame =  GameObject.Find ("CameraGame").GetComponent<Camera>();
+
+		CameraStart.transform.position = CameraGame.transform.position;
+		CameraStart.transform.rotation = CameraGame.transform.rotation;
+
+		CameraStart.orthographicSize = CameraGame.orthographicSize;
+		CameraStart.nearClipPlane = CameraGame.nearClipPlane;
+
+		CameraStart.backgroundColor = CameraGame.backgroundColor;
+
+		CameraGame.enabled = false;
+		//CameraGame.GetComponent<AudioListener> ().enabled = false;
+
+
+		city = (GameObject) Instantiate(Resources.Load("Prefabs/City"), Vector3.zero, Quaternion.identity);
+		city.name = "City";
+
+		player = (GameObject) Instantiate(Resources.Load("Prefabs/Player"), Vector3.zero, Quaternion.identity);
+		player.name = "Player";
+
+		//CameraStart.gameObject.AddComponent<CameraController> ();
+
+		Destroy (CameraGame);
+	}
+
+
+}
